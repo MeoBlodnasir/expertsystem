500b772f5bbfc7300f0e5cb03690f09f93b91153
djo <diccy91@hotmail.com>
2017-01-30 03:27:11 +0100

    Traque de fuites mémoire sur Windows

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,9 +1,20 @@
 
+#if defined(_WIN32)
+#	include <crtdbg.h>
+#endif
+
 #include "Core.h"
 #include "Application.h"
 
+FT_TODO("Voir comment traquer les fuites mémoire sur Linux et Mac aussi.");
+
 int		main(int ac, char **av)
 {
+	// Traque des fuites mémoire
+#if defined(_WIN32)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
 	ft::Application	oApplication;
 
 	FT_TEST_RETURN(oApplication.Init(ac, av) == ft::FT_OK, -1);
