f2bf111df52c111a5f66c1018013c06f2d47edad
djo <diccy91@hotmail.com>
2017-02-01 15:13:49 +0100

    Ajouts d'Atom à valeur contante

--- Code/Atom.h
+++ Code/Atom.h
@@ -24,15 +24,38 @@ namespace ft
 				virtual	ILogicElement*	Duplicate() const override;
 
 		// implémentation interface IProposition
-		virtual bool	Evaluate(const VariablesManager& oVariablesManager) const override;
-		virtual bool	CheckValidity() const override;
-		virtual void	GetAtomsId(AtomIdSet* pIdSet) const override;
+				virtual bool	Evaluate(const VariablesManager& oVariablesManager) const override;
+				virtual bool	CheckValidity() const override;
+				virtual void	GetAtomsId(AtomIdSet* pIdSet) const override;
 
-		inline Id		GetId() const					{ return m_iId; }
-		inline void		SetId(Id iId)					{ m_iId = iId; }
+		inline	Id				GetId() const						{ return m_iId; }
+		inline	virtual void	SetId(Id iId)						{ m_iId = iId; }
 
-	private:
+	protected:
 
 		Id	m_iId;
+	};
+
+	template <bool bValue, Atom::Id iDesc>
+	class ConstantAtom : public Atom
+	{
+	public:
+
+		ConstantAtom() : Atom(iDesc)			{}
+		ConstantAtom(Id) : Atom(iDesc)			{}
+		ConstantAtom(const Atom&) : Atom(iDesc)	{}
+		virtual ~ConstantAtom();
+
+		inline virtual bool	Evaluate(const VariablesManager&) const override	{ return bValue; }
+
+		inline virtual void	SetId(Id) override	{}
+	};
+
+	class AtomTrue : public ConstantAtom<true, '*'>
+	{
+	};
+
+	class AtomFalse : public ConstantAtom<false, '-'>
+	{
 	};
 }
